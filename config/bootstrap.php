f6944c1f4f031558720e6e72735bcabe9129ab07
Leon Plata <pcg.leon@gmail.com>
2017-09-26 14:09:04 -0400

    Added CORS support

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -215,3 +215,5 @@ Type::build('timestamp')
 if (Configure::read('debug')) {
     Plugin::load('DebugKit', ['bootstrap' => true]);
 }
+
+Plugin::load('Cors', ['bootstrap' => true, 'routes' => false]);
